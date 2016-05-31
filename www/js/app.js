83ce1ae3d9b10a527b26a82710b096b98831a8cf
sharmanikita <nik.8687@gmail.com>
2016-05-31 17:29:42 +0530

    Added hero pictures

--- www/js/app.js
+++ www/js/app.js
@@ -7,58 +7,46 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('bollywood', ['ionic', 'bollywood.controllers', 'bollywood.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+      .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('movies', {
+        url: '/movies/:heroId',
+        templateUrl: 'templates/movies.html',
+        controller: 'MoviesCtrl'
+      })
+      .state('movieDetails', {
+        url: '/movieDetails/:movieId',
+        templateUrl: 'templates/movieDetails.html',
+        controller: 'MovieDetailsCtrl'
+      })
+
+    $urlRouterProvider.otherwise('/home');
+
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    .state('home', {
-      url: '/home',
-      templateUrl: 'templates/home.html',
-      controller: 'HomeCtrl'
-    })
-    .state('movies', {
-      url: '/movies/:heroId',
-      templateUrl: 'templates/movies.html',
-      controller: 'MoviesCtrl'
-    })
-    .state('movieDetails', {
-      url: '/movieDetails/:movieId',
-      templateUrl: 'templates/movieDetails.html',
-      controller: 'MovieDetailsCtrl'
-    })
-/*
-    .state('employee', {
-      url: '/employees/:employeeId',
-      templateUrl: 'templates/employee-detail.html',
-      controller: 'EmployeeDetailCtrl'
-    })
-
-    .state('reports', {
-      url: '/employees/:employeeId/reports',
-      templateUrl: 'templates/employee-reports.html',
-      controller: 'EmployeeReportsCtrl'
-    });*/
-
-  $urlRouterProvider.otherwise('/home');
-
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,48 +12,51 @@ angular.module('bollywood.controllers', [])
 
     $scope.heroes = [{
       id: 'b396c2fd',
-      name: 'Salman Khan'
+      name: 'Salman Khan',
+      pic: 'https://s3-ap-southeast-1.amazonaws.com/cinemalytics/person/DFD1E5E17535E6091829DBBD7F78D545.jpg'
     },
       {
         id: 'bdd53ba7',
-        name: 'Shahrukh Khan'
+        name: 'Shahrukh Khan',
+        pic: 'https://s3-ap-southeast-1.amazonaws.com/cinemalytics/person/6BE2F15208E8041CC897FA468AA44512.jpg'
       },
       {
         id: '0dcc2bbb',
-        name: 'Amir Khan'
+        name: 'Amir Khan',
+        pic: 'https://s3-ap-southeast-1.amazonaws.com/cinemalytics/person/FDFD62EE33839A948E7CB371B5B949D4.jpg'
       },
       {
         id: 'e97ded72',
-        name: 'Ranbir Kapoor'
+        name: 'Ranbir Kapoor',
+        pic : 'https://s3-ap-southeast-1.amazonaws.com/cinemalytics/person/A4501C1E4CC1D360715AF0ABEA0ABE6C.jpg'
       },
       {
         id: '9999',
-        name: 'Surprise Me...'
+        name: 'Surprise Me...',
+        pic: '../images/random.png'
       }];
 
-    $scope.setMyHero = function(hero) {
+    $scope.setMyHero = function (hero) {
       $scope.myHero = hero;
     }
 
 
-
-
   })
   .controller('MoviesCtrl', function ($scope, $stateParams, Movies) {
-	  var showMovie = function() {
-		  if ($stateParams.heroId === '9999') {
-			   Movies.getRandomMovie();
-		  } else {
-			  Movies.getMovieByHero($stateParams.heroId);
-		  }
-	  }
-	  
-	  showMovie();
-	  
-	  
-	  $scope.getMoreMovie = function() {
-		  showMovie();
-	  }
+    var showMovie = function () {
+      if ($stateParams.heroId === '9999') {
+        Movies.getRandomMovie();
+      } else {
+        Movies.getMovieByHero($stateParams.heroId);
+      }
+    }
+
+    showMovie();
+
+
+    $scope.getMoreMovie = function () {
+      showMovie();
+    }
   })
-  .controller('MovieDetailsCtrl', function($scope) {
+  .controller('MovieDetailsCtrl', function ($scope) {
   })
